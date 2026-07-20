cd657a7d55eda742dde3ad3cd3f10bf9cf0c7a55
Decap <ssembatyajoseph963@gmail.com>
2026-07-21 00:47:08 +0300

    Revert Admin View back to clean light theme

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -3,16 +3,13 @@ import SideBar from "../Components/SideBar";
 
 import "./layout.css";
 import "./responsive.css";
-import "../styles/adminDarkTheme.css";
 
 export default function AdminLayout({ children }) {
   return (
     <SidebarProvider>
-      <div className="admin-dark-theme">
-        <div className="dashboard-container admin-dashboard">
-          <SideBar />
-          <div className="main-content admin-main-content">{children}</div>
-        </div>
+      <div className="dashboard-container admin-dashboard">
+        <SideBar />
+        <div className="main-content admin-main-content">{children}</div>
       </div>
     </SidebarProvider>
   );
