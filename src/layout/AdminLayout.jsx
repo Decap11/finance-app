09fb00b1d1a9b2057d8f6d5aa0bb31f9564a2b05
Decap <ssembatyajoseph963@gmail.com>
2026-07-21 00:41:22 +0300

    Migrate Admin View to Developer Portal dark glassmorphism theme

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -3,13 +3,16 @@ import SideBar from "../Components/SideBar";
 
 import "./layout.css";
 import "./responsive.css";
+import "../styles/adminDarkTheme.css";
 
 export default function AdminLayout({ children }) {
   return (
     <SidebarProvider>
-      <div className="dashboard-container admin-dashboard">
-        <SideBar />
-        <div className="main-content admin-main-content">{children}</div>
+      <div className="admin-dark-theme">
+        <div className="dashboard-container admin-dashboard">
+          <SideBar />
+          <div className="main-content admin-main-content">{children}</div>
+        </div>
       </div>
     </SidebarProvider>
   );
